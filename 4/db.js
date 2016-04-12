f062cb19b14570483201830bfcb6185f853d9551
Patrick <pingvinen@isflagen.dk>
2016-04-12 14:03:38 +0200

    Added all merchants (that I have met) and the shipments they provide (quantity etc is wrong for many entries at this point)

--- 4/db.js
+++ 4/db.js
@@ -20,6 +20,78 @@ var dbString = JSON.stringify({
 				"location": "Graygarden",
 				"_id": "greene"
 			}
+		}, {
+			"values": {
+				"name": "Daisy",
+				"location": "Goodneighbor",
+				"_id": "daisy"
+			}
+		}, {
+			"values": {
+				"name": "Myrna",
+				"location": "Diamond City",
+				"_id": "myrna"
+			}
+		}, {
+			"values": {
+				"name": "Cricket",
+				"location": "travelling",
+				"_id": "cricket"
+			}
+		}, {
+			"values": {
+				"name": "KLEO",
+				"location": "Goodneighbor",
+				"_id": "kleo"
+			}
+		}, {
+			"values": {
+				"name": "Arturo",
+				"location": "Diamond City",
+				"_id": "arturo"
+			}
+		}, {
+			"values": {
+				"name": "Connie",
+				"location": "Abernathy Farm",
+				"_id": "connie"
+			}
+		}, {
+			"values": {
+				"name": "Moe",
+				"location": "Diamond City",
+				"_id": "moe"
+			}
+		}, {
+			"values": {
+				"name": "Becky Fallon",
+				"location": "Diamond City",
+				"_id": "beckyfallon"
+			}
+		}, {
+			"values": {
+				"name": "Dr. Sun",
+				"location": "Diamond City",
+				"_id": "drsun"
+			}
+		}, {
+			"values": {
+				"name": "Doc Weathers",
+				"location": "Bunker Hill",
+				"_id": "docweathers"
+			}
+		}, {
+			"values": {
+				"name": "Kay",
+				"location": "Bunker Hill",
+				"_id": "kay"
+			}
+		}, {
+			"values": {
+				"name": "Alexis",
+				"location": "Vault 81",
+				"_id": "alexis"
+			}
 		}]
 	}, {
 		"name": "shipments",
@@ -36,11 +108,115 @@ var dbString = JSON.stringify({
 		}],
 		"rows": [{
 			"values": {
+				"item": "Adhesive",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "daisy",
+				"_id": "b01ed561-5916-4216-a6aa-1c4587d89a13"
+			}
+		}, {
+			"values": {
+				"item": "Adhesive",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "myrna",
+				"_id": "7b8bec35-8cd6-4c27-b51d-d0bacd712e0b"
+			}
+		}, {
+			"values": {
+				"item": "Circuitry",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "daisy",
+				"_id": "7e290669-b7bc-4855-a066-a6462af677af"
+			}
+		}, {
+			"values": {
 				"item": "Circuitry",
 				"shipmentQuantity": 25,
 				"quantity": 1,
 				"merchantId": "trashcancarla",
-				"_id": "uuid1"
+				"_id": "529ff195-7dc9-41f2-86e7-656a568a78fc"
+			}
+		}, {
+			"values": {
+				"item": "Crystal",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "daisy",
+				"_id": "98a13f19-4b8a-4d06-924e-04eb02a536d4"
+			}
+		}, {
+			"values": {
+				"item": "Crystal",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "trashcancarla",
+				"_id": "f16c7710-abfb-45a4-9625-25755184a303"
+			}
+		}, {
+			"values": {
+				"item": "Silver",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "daisy",
+				"_id": "3bfe9f30-9c15-4b0a-88ae-2acbe76f6eda"
+			}
+		}, {
+			"values": {
+				"item": "Silver",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "cricket",
+				"_id": "db4f9826-17f2-4768-92cd-cf81680d9aa9"
+			}
+		}, {
+			"values": {
+				"item": "Ballistic fiber",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "kleo",
+				"_id": "fc0a7362-0d04-4201-ad92-33f2ef0f972e"
+			}
+		}, {
+			"values": {
+				"item": "Copper",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "kleo",
+				"_id": "3200052f-2b2b-4389-84ff-68abb01d3dfe"
+			}
+		}, {
+			"values": {
+				"item": "Copper",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "arturo",
+				"_id": "cf17fa63-3e43-4e4d-87f6-6581ad713cb3"
+			}
+		}, {
+			"values": {
+				"item": "Lead",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "kleo",
+				"_id": "8a7ec98d-3253-46a2-9549-7e14c3471521"
+			}
+		}, {
+			"values": {
+				"item": "Lead",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "cricket",
+				"_id": "90f8a944-216a-47c5-a3f3-3181f90c8469"
+			}
+		}, {
+			"values": {
+				"item": "Oil",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "kleo",
+				"_id": "b2946f8b-ade0-4925-bc29-1fede086b443"
 			}
 		}, {
 			"values": {
@@ -48,7 +224,71 @@ var dbString = JSON.stringify({
 				"shipmentQuantity": 25,
 				"quantity": 1,
 				"merchantId": "trashcancarla",
-				"_id": "uuid2"
+				"_id": "3c5653a4-a4cf-41d8-8992-5bfe9a8ab769"
+			}
+		}, {
+			"values": {
+				"item": "Aluminum",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "arturo",
+				"_id": "559bd422-6e18-4863-bff1-8678430b73ff"
+			}
+		}, {
+			"values": {
+				"item": "Gears",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "arturo",
+				"_id": "98402ab9-b465-480a-8269-f5dc51a5b507"
+			}
+		}, {
+			"values": {
+				"item": "Gears",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "cricket",
+				"_id": "0b0a1c43-5285-4b6a-beea-3b6fde58084b"
+			}
+		}, {
+			"values": {
+				"item": "Screws",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "arturo",
+				"_id": "15adfad3-a264-4d7a-80de-074d7b7fbe6e"
+			}
+		}, {
+			"values": {
+				"item": "Screws",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "trashcancarla",
+				"_id": "ec7eb144-080c-4d30-9eb3-605616677f25"
+			}
+		}, {
+			"values": {
+				"item": "Ceramic",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "myrna",
+				"_id": "4281ca99-efd5-4774-bdec-bada5cc2f3ee"
+			}
+		}, {
+			"values": {
+				"item": "Fertilizer",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "myrna",
+				"_id": "83271366-ad44-4e7d-bf18-5603fb5e10b1"
+			}
+		}, {
+			"values": {
+				"item": "Fertilizer",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "connie",
+				"_id": "6bcb27ca-a75e-4f83-aa58-4a2c70f9e01d"
 			}
 		}, {
 			"values": {
@@ -56,7 +296,223 @@ var dbString = JSON.stringify({
 				"shipmentQuantity": 25,
 				"quantity": 1,
 				"merchantId": "greene",
-				"_id": "uuid3"
+				"_id": "d92df2fd-674a-4154-8cd6-f55d42dd0f12"
+			}
+		}, {
+			"values": {
+				"item": "Plastic",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "myrna",
+				"_id": "ec21b992-3de6-4e82-b650-813139ef80fa"
+			}
+		}, {
+			"values": {
+				"item": "Springs",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "myrna",
+				"_id": "bcd1c1da-3a36-4a8d-813d-98047515ba12"
+			}
+		}, {
+			"values": {
+				"item": "Springs",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "cricket",
+				"_id": "7e3ca046-d2b1-407c-a4d9-e02cc0b037e8"
+			}
+		}, {
+			"values": {
+				"item": "Steel",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "myrna",
+				"_id": "1c427da6-c45d-4bb6-8226-289b6943384b"
+			}
+		}, {
+			"values": {
+				"item": "Cork",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "moe",
+				"_id": "62bb937b-8e38-4ad2-bd6c-3fd138706a24"
+			}
+		}, {
+			"values": {
+				"item": "Wood",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "moe",
+				"_id": "04e92b30-4c0b-487d-9cc1-6fabae6b59ad"
+			}
+		}, {
+			"values": {
+				"item": "Wood",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "connie",
+				"_id": "25699529-c1da-4b18-a2e7-f5b7837b148f"
+			}
+		}, {
+			"values": {
+				"item": "Wood",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "greene",
+				"_id": "24db3943-e50c-4b90-8c02-0fe3546648de"
+			}
+		}, {
+			"values": {
+				"item": "Cloth",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "beckyfallon",
+				"_id": "36f9d4fb-6f10-4e42-85d5-e6a8739bf2b0"
+			}
+		}, {
+			"values": {
+				"item": "Cloth",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "connie",
+				"_id": "ca092c87-926e-493b-8895-4d04596e8252"
+			}
+		}, {
+			"values": {
+				"item": "Cloth",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "greene",
+				"_id": "a06838fa-2e6c-4b8c-bc96-1a16f8f299c2"
+			}
+		}, {
+			"values": {
+				"item": "Leather",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "beckyfallon",
+				"_id": "07fb2156-de67-45d1-85b5-d294f668389f"
+			}
+		}, {
+			"values": {
+				"item": "Leather",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "connie",
+				"_id": "727066f1-3325-478f-b25c-a53d6f9ddcdf"
+			}
+		}, {
+			"values": {
+				"item": "Leather",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "greene",
+				"_id": "e000d964-a8a6-4329-ad9e-615610324e66"
+			}
+		}, {
+			"values": {
+				"item": "Antiseptic",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "drsun",
+				"_id": "62d11180-2195-47ae-af6a-029fcdf0ecaa"
+			}
+		}, {
+			"values": {
+				"item": "Antiseptic",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "docweathers",
+				"_id": "685b1d49-035d-4173-9b1f-03d8a316d088"
+			}
+		}, {
+			"values": {
+				"item": "Glass",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "drsun",
+				"_id": "e7ff3fc2-1909-47ca-b6b9-f4d93249206a"
+			}
+		}, {
+			"values": {
+				"item": "Glass",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "docweathers",
+				"_id": "757ffc95-a6b3-4911-93ae-61f9a8fbbc30"
+			}
+		}, {
+			"values": {
+				"item": "Glass",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "kay",
+				"_id": "ad102ba2-493d-426c-9d62-2ae269ff35a2"
+			}
+		}, {
+			"values": {
+				"item": "Rubber",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "drsun",
+				"_id": "479053b4-4e7f-45b7-b649-9487ac9e8ad6"
+			}
+		}, {
+			"values": {
+				"item": "Rubber",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "docweathers",
+				"_id": "34a560fa-a1a3-421e-9127-5997e35c999d"
+			}
+		}, {
+			"values": {
+				"item": "Acid",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "docweathers",
+				"_id": "afa50a51-11d3-4921-be74-483ccec84cdc"
+			}
+		}, {
+			"values": {
+				"item": "Acid",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "kay",
+				"_id": "1d60a602-f818-436f-9ed7-340c7fba6b82"
+			}
+		}, {
+			"values": {
+				"item": "Nuclea Material",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "alexis",
+				"_id": "cd0b4917-5649-474d-a2b8-e3c077f90c99"
+			}
+		}, {
+			"values": {
+				"item": "Asbestos",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "trashcancarla",
+				"_id": "8680fd8b-fe9d-4c0b-9228-1cf5df094bc0"
+			}
+		}, {
+			"values": {
+				"item": "Concrete",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "connie",
+				"_id": "5400e619-cf21-4019-9b30-48e83f6501f1"
+			}
+		}, {
+			"values": {
+				"item": "Concrete",
+				"shipmentQuantity": 25,
+				"quantity": 1,
+				"merchantId": "greene",
+				"_id": "c3eb2463-c342-4fe4-93ad-b2f23d6fbb5e"
 			}
 		}]
 	}]
